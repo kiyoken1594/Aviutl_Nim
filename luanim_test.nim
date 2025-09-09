4decbeb7a82c4921aefb9dac671ccd015b8004f1
kiyoken1594 <kiyokenchoukai1594@outlook.jp>
2025-09-09 23:21:18 +0900

    Write concrete process, and delete prototype

--- luanim_test.nim
+++ luanim_test.nim
@@ -1,15 +1,38 @@
-import luanim/lua
+import luanim/luanim
 import nimforAviUtl
 
-# Define a function that adds two numbers
-makeAviUtlfunc(add_numbers, luanim_test):
-  # Complete procedure definition including L parameter
-  proc add_numbers(L: ptr lua_State): cint {.cdecl, exportc.} =
-    let 
-      num1 = lua_tonumber(L, 1)
-      num2 = lua_tonumber(L, 2)
+# (funcname, this_filename)
+makeAviUtlfunc(test_func, luanim_test):
+  # process
+  # example, halve the luminance
+  proc test_func(L: ptr lua_State): cint {.cdecl, exportc.} =
+    # get data of the flame
+    var data = cast[ptr UncheckedArray[uint32]](lua_touserdata(L, 1))
+    let width = uint32(lua_tonumber(L, 2))
+    let height = uint32(lua_tonumber(L, 3))
     
-    lua_pushinteger(L, lua_Integer(num1 + num2))
+    # set stack-top to data
+    lua_settop(L, 1)
+
+    # get and edit pixel
+    for i in 0..<(width * height):
+      # get color data
+      let
+        red = uint8((data[i] shr 16) and 0xFF)
+        green = uint8((data[i] shr 8) and 0xFF)
+        blue = uint8(data[i] and 0xFF)
+        alpha = uint8((data[i] shr 24) and 0xFF)
+      
+      #edit color data
+      let
+        new_red = red div 2
+        new_green = green div 2
+        new_blue = blue div 2
+      
+      # write to data
+      data[i] = ((uint32(new_red) shl 16) or (uint32(new_green) shl 8) or uint32(new_blue) or uint32(alpha) shl 24)
+      
+    # send stack value
     return 1
 
 #[コンパイルオプション
@@ -19,11 +42,20 @@ nim.cfgに
 #Compiler = Visual Studio(x86)
 cc = vcc
 cpu = "i386"
+#Runtime check
+--objChecks:off
+--fieldChecks:off
+--rangeChecks:off
+--boundChecks:off
+--overflowChecks:off
+--floatChecks:off
+--nanChecks:off
+--infChecks:off
 #dll option
 --app: lib
 --nomain
 #Option for compiler
-passC = "/LD"
+passC = "/LD "
 passL = "lua51.lib"
 #memory
 --mm:orc
